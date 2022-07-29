8abdb0fc19e23cb5a5df085f1a633e61e896323e
Andrea Sorrentino <andrea.sorrentino2@studenti.unimi.it>
2022-07-29 13:11:22 +0200

    first update

---
+++ app/lib/minSetCover.py
@@ -0,0 +1,87 @@
+import networkx as nx
+import csv
+from lib.fromCsvToGraph import drawGraph 
+
+#create X from edge to respect the paper S is a subSet of X
+def makeXForAlgo(listoOfNodes,Graph):
+	X = []
+	for node in listoOfNodes:
+        #uso > 0 di zero perché devo prendermi solo le metriche 
+        # da cui parte almeno 1 arco
+		if len(Graph.out_edges(node)) > 0:
+			X.append(node)
+
+	return set(X)
+
+#create S
+def makeSForAlgo(listoOfNodes,Graph):
+	S = []
+	for node in listoOfNodes:
+		tmpList = []
+		for el in Graph.in_edges(node):
+			tmpList.append(el[0])
+		S.append(tmpList)
+	return S
+
+def greedyMinSetCover(X,S):
+	I = set({})
+	while X != set():
+		valMax = 0
+		index = 0
+		for s in S:
+			d = len(set(s).intersection(X))
+			if d > valMax:
+				valMax = d
+				index = S.index(s)
+		
+		print(valMax,index)
+	
+		I = I.union(set({index}))
+		X = X.difference(set(S[index]))
+		
+	return I
+
+def exeMinSetCoverV1(fileNodes,MGM,outputFileName):
+    listOfMetrics = []
+    listOfClusters = []
+    listOfInputs = []
+
+    for file in fileNodes:
+        with open(file, 'r') as db:
+            csvreader = csv.reader(db)
+            for row in csvreader:
+                if 'METRICS.csv' in file:
+                    listOfMetrics.append(row[0])
+                elif 'CLUSTERS.csv' in file:
+                    listOfClusters.append(row[0])
+                else:
+                    listOfInputs.append(row[0])
+    
+    subMGM = MGM.subgraph(listOfMetrics+listOfClusters)
+   
+    listOfClusters = list(set(listOfClusters))
+   
+    setMetrics	= set(listOfMetrics)
+    setClusters	= set(listOfClusters)
+    setInputs	= set(listOfInputs)
+    
+    #SICCOME POSSO AVERE METRICHE CHE NON SONO COLLEGATE A NULLA
+    #PRENDO SOLO LE METRICHE CON UN ARCO USCENTE
+    #PERCIò creo X e S
+
+    S = makeSForAlgo(listOfClusters,subMGM)
+    X = makeXForAlgo(listOfMetrics,subMGM)
+    
+    I = greedyMinSetCover(X, S)
+
+    listOfCovCluster = [listOfClusters[el] for el in I]
+
+    print(len(listOfClusters),len(listOfCovCluster))
+
+    eff = (len(listOfCovCluster)/len(listOfClusters))*100
+
+    print('Col {}% di cluster riesco a coprire il 100% di metriche che hanno almeno un arco uscente '.format(str(eff)))
+    
+    covGraph_v1 = MGM.subgraph(listOfMetrics+listOfCovCluster)
+
+    drawGraph(covGraph_v1, outputFileName)
---
+++ app/main.py
@@ -0,0 +1,66 @@
+import configparser
+import networkx as nx
+
+from lib.convertXlsToCSV import convertXlsToCSV
+from lib.fromCsvToGraph import genXmlGraph
+from lib.fromCsvToGraph import genPosNodes
+from lib.fromCsvToGraph import drawGraph
+
+from lib.minSetCover import exeMinSetCoverV1
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+########################################################################################
+#STEP 1 convert XLS DB TO CSV
+
+pathDbFiles			=	config['DB']['pathDbFiles']
+xlsDbPathFile		=	config['DB']['pathDbFiles']+config['DB']['xlsFileName']
+sheetNames			=	config['DB']['sheetsName'].split(',')
+outputPathDbFiles	=	config['DB']['pathDbFiles']
+
+
+convertXlsToCSV(sheetNames,xlsDbPathFile,outputPathDbFiles)
+
+########################################################################################
+#STEP 2 convert CSV files in XML file for networkx lib
+
+fileNodes		=	[pathDbFiles+a for a in config['GRAPH']['nodeNames'].split(',')]
+fileEdges		=	[pathDbFiles+a for a in config['GRAPH']['edgeNames'].split(',')]
+fileGraphName	=	config['GRAPH']['xmlGraphName']
+
+
+genXmlGraph(fileNodes, fileEdges, fileGraphName)
+
+########################################################################################
+#STEP 3 make position node file to see a good graph
+delta			=	[int(x) for x in config['GRAPH']['delta'].split(',')]
+pos			=	[int(x) for x in config['GRAPH']['pos'].split(',')]
+filePosName	=	config['GRAPH']['filePosName']
+
+genPosNodes(fileNodes, delta, pos, filePosName)
+
+########################################################################################
+#STEP 4 draw the graph
+
+outputPath		=	config['GRAPH']['outputPath']
+outputFigGraph	=	outputPath+'MGM.pdf'
+
+MGM = nx.read_graphml(fileGraphName)
+
+drawGraph(MGM, outputFigGraph)
+
+
+########################################################################################
+#STEP 5 - test MinSetCov v1
+
+outputFile	=	outputPath+'MinSetCov_v1.pdf'
+
+exeMinSetCoverV1(fileNodes, MGM, outputFile)
+
+
+
+########################################################################################
+print('ok')
+
